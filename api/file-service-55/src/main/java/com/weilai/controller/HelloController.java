2c48f03ea6f55f21224f62e78750732e4be18b5f
miku-wwl <569144003@qq.com>
2024-07-04 00:59:59 +0800

    feat: 4-10 实现微服务网关Gateway的统一路由功能

--- api/file-service-55/src/main/java/com/weilai/controller/HelloController.java
+++ api/file-service-55/src/main/java/com/weilai/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("a")
+@RequestMapping("f")
 public class HelloController {
     @GetMapping("hello")
     public Object hello() {
--- api/main-service-66/src/main/java/com/weilai/controller/HelloController.java
+++ api/main-service-66/src/main/java/com/weilai/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("a")
+@RequestMapping("m")
 public class HelloController {
     @GetMapping("hello")
     public Object hello() {
