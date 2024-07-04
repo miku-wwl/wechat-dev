b839f70b3480442fa780ba437a384a80eece1fa8
miku-wwl <569144003@qq.com>
2024-07-05 00:11:30 +0800

    feat: 5-5 微服务集成云短信功能

--- api/auth-service-88/src/main/java/com/weilai/controller/HelloController.java
+++ api/auth-service-88/src/main/java/com/weilai/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.weilai.controller;
 
+import com.weilai.utils.MyInfo;
+import com.weilai.utils.SMSUtils;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("a")
 public class HelloController {
+
+    @Resource
+    private SMSUtils smsUtils;
+
     @GetMapping("hello")
     public Object hello() {
         return "Hello world~";
+    }
+
+    @GetMapping("sms")
+    public Object sms() throws Exception {
+
+        // 腾讯云短信未开通
+        // smsUtils.sendSMS(MyInfo.getMobile(), "9875");
+        return "Send SMS OK~";
     }
 }
--- wechat-common/src/main/java/com/weilai/utils/TencentCloudProperties.java
+++ wechat-common/src/main/java/com/weilai/utils/TencentCloudProperties.java
@@ -1,20 +1,17 @@
-//package com.weilai.utils;
-//
-//import lombok.Data;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.PropertySource;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * @Auther 风间影月
-// */
-//@Component
-//@Data
-//@PropertySource("classpath:tencentCloud.properties")
-//@ConfigurationProperties(prefix = "tencent.cloud")
-//public class TencentCloudProperties {
-//
-//    private String SecretId;
-//    private String SecretKey;
-//
-//}
+package com.weilai.utils;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@PropertySource("classpath:tencentCloud.properties")
+@ConfigurationProperties(prefix = "tencent.cloud")
+public class TencentCloudProperties {
+
+    private String SecretId;
+    private String SecretKey;
+
+}
